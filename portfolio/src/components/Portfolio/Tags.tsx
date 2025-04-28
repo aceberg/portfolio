5984cdf0118f952d799b9d63a268e782ab2c05eb
aceberg <1502200+aceberg@users.noreply.github.com>
2025-04-28 22:10:03 +0700

    Tags table

--- portfolio/src/components/Portfolio/Tags.tsx
+++ portfolio/src/components/Portfolio/Tags.tsx
@@ -4,7 +4,34 @@ function Tags() {
 
   return (
     <>
-    <p>Tags will be here</p>
+    <div class="overflow-x-auto mt-2 mb-8">
+      <table class="min-w-full table-auto divide-y divide-gray-700 bg-gray-900 rounded-xl overflow-hidden">
+        <thead class="bg-gray-800">
+          <tr>
+            <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-gray-300 uppercase tracking-wider">Backend</th>
+            <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-gray-300 uppercase tracking-wider">Frontend</th>
+            <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-gray-300 uppercase tracking-wider">Other</th>
+          </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-800">
+          <tr>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+              <li>Go (gin, InfluxDB, PostgreSQL)</li>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+              <li>JavaScript, TypeScript</li>
+              <li>React (MobX), SolidJS</li>
+              <li>Bootstrap, Tailwind CSS</li>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+              <li>Flutter (Android)</li>
+              <li>Firefox Extension</li>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
     </>
   )
 }
